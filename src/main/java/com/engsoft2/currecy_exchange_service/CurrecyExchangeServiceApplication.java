c263e5a318e4780a5bb7feca2269af4b213545ac
eduardaschuler <dudaschuler2011@gmail.com>
2025-08-27 19:41:52 -0300

    commit inicial de estrutura

---
+++ src/main/java/com/engsoft2/currecy_exchange_service/CurrecyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.engsoft2.currecy_exchange_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrecyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrecyExchangeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engsoft2/currecy_exchange_service/CurrecyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engsoft2.currecy_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrecyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
