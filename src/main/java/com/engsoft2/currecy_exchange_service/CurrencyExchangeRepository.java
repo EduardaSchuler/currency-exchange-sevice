68b2a6ea75dd0951a1f9be11c737317101bdde8b
eduardaschuler <dudaschuler2011@gmail.com>
2025-08-27 19:58:22 -0300

    adiona classes base

---
+++ src/main/java/com/engsoft2/currecy_exchange_service/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.engsoft2.currecy_exchange_service;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyExchangeRepository extends
+        CrudRepository<CurrencyExchange, Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
